fd9f8e25f8cd66e6d56cc13a444d6ed39fddf394
Daviddlls <david.dlls05@gmail.com>
2025-07-29 17:20:07 -0500

    Versión nueva de mi backend

--- index.js
+++ index.js
@@ -19,9 +19,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
-  credentials: true,
+  origin: '*',
 }));
+
+
 app.use(express.json());
 
 // Rutas principales
